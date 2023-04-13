ebd192f404f2f9149017a903cb0e0aecd75ec222
Keidbyr <117668622+Keidbyr@users.noreply.github.com>
2023-04-13 16:51:24 +0500

    Задания

---
+++ Web знакомство с Api/3.py
@@ -0,0 +1,14 @@
+import requests
+import json
+from api import apikey
+
+city_list = ['Барнаул', 'Мелеуз', 'Йошкар-Ола']
+coords = []
+
+for city in city_list:
+    url = f'https://geocode-maps.yandex.ru/1.x/?apikey={apikey}&format=json&geocode={city}'
+    response = requests.get(url).text
+    data = json.loads(response)
+    coords.append(data['response']['GeoObjectCollection']['featureMember'][0]['GeoObject']["metaDataProperty"]["GeocoderMetaData"]["Address"]["Components"][1]["name"])
+
+print(coords)
